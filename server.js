562673f85eaa4fde2e2b6c8e6c9c5a79951c46a1
Faithful Olaleru <faithfulolaleru09@gmail.com>
2023-01-20 13:13:02 +0100

    initial commit + setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv")
+
+
+const app = express();
+
+dotenv.config()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
